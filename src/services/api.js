558674c7fc396f805109dbae7392d3a8dcdef12e
Ijlal Furqaan <45873251+IjlalFurqaan@users.noreply.github.com>
2024-09-06 09:31:50 +0200

    removed env file

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,4 @@
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
-
+const API_BASE_URL = "https://dev.brain-backend.dainstudios.com/challenge";
 
 export async function fetchChatSessions(startDate, endDate) {
   let url = `${API_BASE_URL}/chat_sessions`;
